dcee7ee56741acdd639e0a7748313ca06fbd2e07
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-08-31 10:48:34 +0700

    Update data_sources.py

--- config/data_sources.py
+++ config/data_sources.py
@@ -1,3 +1,3 @@
 EXCEL_URLS = {
-    "financial": "https://...."
+    "financial": "https://docs.google.com/spreadsheets/d/1NFZ6zPs7g6RoyBHuroSWXvmd3W0vOs4B/edit?usp=sharing&ouid=111125876053982824420&rtpof=true&sd=true"
 }
