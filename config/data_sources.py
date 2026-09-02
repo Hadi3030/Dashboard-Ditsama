7da09d08339a5096179f54752ea67402ec48d215
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-09-02 14:40:14 +0700

    Update data_sources.py

--- config/data_sources.py
+++ config/data_sources.py
@@ -1,3 +1,5 @@
 EXCEL_URLS = {
-    "financial": "https://docs.google.com/spreadsheets/d/1NFZ6zPs7g6RoyBHuroSWXvmd3W0vOs4B/edit?usp=sharing&ouid=111125876053982824420&rtpof=true&sd=true"
+    "financial": "https://docs.google.com/spreadsheets/d/1NFZ6zPs7g6RoyBHuroSWXvmd3W0vOs4B/edit?usp=sharing&ouid=111125876053982824420&rtpof=true&sd=true",
+
+    "participant": "https://hil20709853-my.sharepoint.com/:x:/g/personal/rehanda_getcid_id/IQBtYnorlx4TQpEt3Zast6Q-AZ37tobHB7BsOw8L228Pcyk?e=SDHyhb"
 }
