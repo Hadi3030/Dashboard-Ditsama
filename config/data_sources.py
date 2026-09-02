64529fd9e57ccdc2f69ca99a063ecc0793e09703
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-09-02 14:49:22 +0700

    Update data_sources.py

--- config/data_sources.py
+++ config/data_sources.py
@@ -1,5 +1,32 @@
-EXCEL_URLS = {
-    "financial": "https://docs.google.com/spreadsheets/d/1NFZ6zPs7g6RoyBHuroSWXvmd3W0vOs4B/edit?usp=sharing&ouid=111125876053982824420&rtpof=true&sd=true",
+# =========================================================
+# DATA SOURCES
+# Dashboard Program Ditsama 2026
+# =========================================================
 
-    "participant": "https://hil20709853-my.sharepoint.com/:x:/g/personal/rehanda_getcid_id/IQBtYnorlx4TQpEt3Zast6Q-AZ37tobHB7BsOw8L228Pcyk?e=SDHyhb"
+
+EXCEL_URLS = {
+
+    # =====================================================
+    # DATA 1
+    # FINANCIAL PERFORMANCE
+    # =====================================================
+
+    "financial":
+        "https://docs.google.com/spreadsheets/d/"
+        "1NFZ6zPs7g6RoyBHuroSWXvmd3W0vOs4B/"
+        "edit?usp=sharing&ouid=111125876053982824420&rtpof=true&sd=true",
+
+
+    # =====================================================
+    # DATA 2
+    # PARTICIPANT TARGET & ACTUAL
+    # SIAP + INSPIRASI
+    # =====================================================
+
+    "participant":
+        "https://hil20709853-my.sharepoint.com/"
+        ":x:/g/personal/rehanda_getcid_id/"
+        "IQBtYnorlx4TQpEt3Zast6Q-AZ37tobHB7BsOw8L228Pcyk"
+        "?e=SDHyhb"
+
 }
